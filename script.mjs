9e38e0832dae5af48d7ab615839835e58a7af7b0
Safa Shahin <safashahin02@icloud.com>
2025-01-15 18:07:59 +0100

    La til rutene /tmp/poem og /tmp/quote

--- script.mjs
+++ script.mjs
@@ -13,6 +13,39 @@ function getRoot(req, res, next) {
 
 server.get("/", getRoot);
 
+
+// Rute for "/tmp/poem"
+function getPoem(req, res, next) {
+    const poem = `
+        Roses are red,
+        Violets are blue,
+        Express is fun,
+        And so are you.
+    `;
+    res.status(HTTP_CODES.SUCCESS.OK).send(poem).end();
+}
+
+server.get("/tmp/poem", getPoem);
+
+// Rute for "/tmp/quote"
+function getQuote(req, res, next) {
+    const quotes = [
+        " The only limit to our realization of tomorrow is our doubts of today.  Franklin D. Roosevelt",
+        "Do not watch the clock. Do what it does. Keep going.  Sam Levenson",
+        "The future belongs to those who believe in the beauty of their dreams.  Eleanor Roosevelt",
+        "It always seems impossible until its done.  Nelson Mandela",
+        "Believe you can and you're halfway there.  Theodore Roosevelt"
+    ];
+
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+
+    res.status(HTTP_CODES.SUCCESS.OK).send(randomQuote).end();
+}
+
+server.get("/tmp/quote", getQuote);
+
+// Start serveren
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 }); // Dette er en test
