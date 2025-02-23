ab417b2b7133cefc99c5924786ddf96e3a9db392
Safa Shahin <safashahin02@icloud.com>
2025-02-23 02:18:13 +0100

    Add tree data structure, CRUD controller, and routes for assignment 7

---
+++ localLibrary/data/treeData.js
@@ -0,0 +1,9 @@
+export let treeData = {
+    id: 1,
+    name: "Root",
+    children: [
+      { id: 2, name: "Child 1", children: [] },
+      { id: 3, name: "Child 2", children: [] }
+    ]
+  };
+  
